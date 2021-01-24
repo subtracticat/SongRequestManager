7c4023577978bde43c0e7025c6758156a151e22a
Subtracticat <subtracticat@outlook.com>
2021-01-24 10:08:43 -0800

    Disable keyboard

--- EnhancedTwitchIntegration/UI/KeyboardViewController.cs
+++ EnhancedTwitchIntegration/UI/KeyboardViewController.cs
@@ -9,45 +9,45 @@ namespace SongRequestManager.UI
         {
             if (firstActivation)
             {
-                RectTransform KeyboardContainer = new GameObject("KeyboardContainer", typeof(RectTransform)).transform as RectTransform;
-                KeyboardContainer.SetParent(rectTransform, false);
-                KeyboardContainer.sizeDelta = new Vector2(60f, 40f);
+//                RectTransform KeyboardContainer = new GameObject("KeyboardContainer", typeof(RectTransform)).transform as RectTransform;
+//                KeyboardContainer.SetParent(rectTransform, false);
+//                KeyboardContainer.sizeDelta = new Vector2(60f, 40f);
 
-                var mykeyboard = new KEYBOARD(KeyboardContainer, "");
+//                var mykeyboard = new KEYBOARD(KeyboardContainer, "");
 
-#if UNRELEASED
-                RequestBot.AddKeyboard(mykeyboard, "emotes.kbd", 0.4f);
-#endif
-                mykeyboard.AddKeys(KEYBOARD.QWERTY); // You can replace this with DVORAK if you like
-                mykeyboard.DefaultActions();
+//#if UNRELEASED
+//                RequestBot.AddKeyboard(mykeyboard, "emotes.kbd", 0.4f);
+//#endif
+//                mykeyboard.AddKeys(KEYBOARD.QWERTY); // You can replace this with DVORAK if you like
+//                mykeyboard.DefaultActions();
 
-#if UNRELEASED
-                const string SEARCH = @"
+//#if UNRELEASED
+//                const string SEARCH = @"
 
-[CLEAR SEARCH]/0 /2 [NEWEST]/0 /2 [UNFILTERED]/30 /2 [PP]/0'!addsongs/top/pp pp%CR%' /2 [SEARCH]/0";
+//[CLEAR SEARCH]/0 /2 [NEWEST]/0 /2 [UNFILTERED]/30 /2 [PP]/0'!addsongs/top/pp pp%CR%' /2 [SEARCH]/0";
 
-#else
-                const string SEARCH = @"
+//#else
+//                const string SEARCH = @"
 
-[CLEAR SEARCH]/0 /2 [NEWEST]/0 /2 [UNFILTERED]/30 /2 [SEARCH]/0";
+//[CLEAR SEARCH]/0 /2 [NEWEST]/0 /2 [UNFILTERED]/30 /2 [SEARCH]/0";
 
-#endif
+//#endif
 
 
-                mykeyboard.SetButtonType("Q"); // Adding this alters button positions??! Why?
-                mykeyboard.AddKeys(SEARCH, 0.75f);
+//                mykeyboard.SetButtonType("Q"); // Adding this alters button positions??! Why?
+//                mykeyboard.AddKeys(SEARCH, 0.75f);
 
-                mykeyboard.SetAction("CLEAR SEARCH", RequestBot.ClearSearch);
-                mykeyboard.SetAction("UNFILTERED", RequestBot.UnfilteredSearch);
-                mykeyboard.SetAction("SEARCH", RequestBot.MSD);
-                mykeyboard.SetAction("NEWEST", RequestBot.Newest);
+//                mykeyboard.SetAction("CLEAR SEARCH", RequestBot.ClearSearch);
+//                mykeyboard.SetAction("UNFILTERED", RequestBot.UnfilteredSearch);
+//                mykeyboard.SetAction("SEARCH", RequestBot.MSD);
+//                mykeyboard.SetAction("NEWEST", RequestBot.Newest);
 
-#if UNRELEASED
-                RequestBot.AddKeyboard(mykeyboard, "decks.kbd", 0.4f);
-#endif
+//#if UNRELEASED
+//                RequestBot.AddKeyboard(mykeyboard, "decks.kbd", 0.4f);
+//#endif
 
-                // The UI for this might need a bit of work.
-                RequestBot.AddKeyboard(mykeyboard, "RightPanel.kbd");
+//                // The UI for this might need a bit of work.
+//                RequestBot.AddKeyboard(mykeyboard, "RightPanel.kbd");
             }
         }
     }

dd24e6cd47dbe4ac7a304b5dd13c8caef4991d0b
Subtracticat <subtracticat@outlook.com>
2023-12-03 23:27:43 -0800

    Fix MaximumSongLength default value

--- SongRequestManager/Config/RequestBotSettings.cs
+++ SongRequestManager/Config/RequestBotSettings.cs
@@ -8,7 +8,7 @@ namespace SongRequestManager.Config
         public int SessionResetAfterXHours = 6; // Number of hours before persistent session properties are reset (ie: Queue, Played , Duplicate List)
 
         public float LowestAllowedRating = 0; // Lowest allowed song rating to be played 0-100 *IMPLEMENTED*, needs UI
-        public float MaximumSongLength = 180; // Maximum song length in minutes
+        public float MaximumSongLength = 6.0f; // Maximum song length in minutes
         public bool SendNextSongBeingPlayedtoChat = true; // Enable chat message when you hit play
         public bool UpdateQueueStatusFiles = true; // Create and update queue list and open/close status files for OBS *IMPLEMENTED*, needs UI
         
